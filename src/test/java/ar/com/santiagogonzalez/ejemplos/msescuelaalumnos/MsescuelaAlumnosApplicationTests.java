6a5ea43bd2c4a8c3c3953bbf1c8a28f65772d596
Santiago Gonzalez <sangonzalez.94@gmail.com>
2019-06-07 01:22:00 -0300

    version inicial de msescuela-alumnos

---
+++ src/test/java/ar/com/santiagogonzalez/ejemplos/msescuelaalumnos/MsescuelaAlumnosApplicationTests.java
@@ -0,0 +1,16 @@
+package ar.com.santiagogonzalez.ejemplos.msescuelaalumnos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class MsescuelaAlumnosApplicationTests {
+
+	/*@Test
+	public void contextLoads() {
+	}*/
+
+}
